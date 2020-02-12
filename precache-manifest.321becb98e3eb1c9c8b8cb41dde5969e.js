3a2d7feb36d33cf45257bab16ce2b2c1706b29b1
Elbshat <mahmoudelbshat7@gmail.com>
2020-02-12 19:38:58 +0200

    Updates

---
+++ precache-manifest.321becb98e3eb1c9c8b8cb41dde5969e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7298e5776962d38e7b71bfb9440f78b2",
+    "url": "/7even/index.html"
+  },
+  {
+    "revision": "5d0f6a689eeb1e38c2d2",
+    "url": "/7even/static/css/main.eb57ac93.chunk.css"
+  },
+  {
+    "revision": "f3361c1f9a66bf3ce6e7",
+    "url": "/7even/static/js/2.29b66cd8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/7even/static/js/2.29b66cd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d0f6a689eeb1e38c2d2",
+    "url": "/7even/static/js/main.989cee6d.chunk.js"
+  },
+  {
+    "revision": "13a6187bc93f21b89930",
+    "url": "/7even/static/js/runtime-main.ef75cded.js"
+  }
+]);
